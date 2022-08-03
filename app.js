f42e347d56a4859a952b3f8caa6b61e753b75e15
Kal O <koestreicher4@gmail.com>
2022-08-03 15:58:03 -0500

    class

---
+++ app.js
@@ -0,0 +1,9 @@
+const inquirer = require('inquirer');
+const Manager = require("./lib/Manager");
+const Employee = require('./lib/Employee');
+const generateSite = require('./src/generate-site.js');
+const fs = require('fs');
+const path = require('path');
+const teamMembers = [];
+
+
