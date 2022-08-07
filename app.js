d3b9844d0cdca57c7fd837e3b1e7ab05859ce6a9
Kal O <koestreicher4@gmail.com>
2022-08-07 17:07:16 -0500

    prompting

--- app.js
+++ app.js
@@ -4,6 +4,39 @@ const Employee = require('./lib/Employee');
 const generateSite = require('./src/generate-site.js');
 const fs = require('fs');
 const path = require('path');
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
 const teamMembers = [];
+
+const promptManager = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+            validate: nameInpute => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'employeeId',
+            message: 'Enter employee ID',
+            validate: employeeId => {
+                if (employeeId) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+    ]
+}
+
 
 
